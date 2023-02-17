85fe4dda8f2984f3064e8f42bc0c62e4f75a2907
dmartimarti <dmartimarti@gmail.com>
2023-02-17 17:31:17 +0000

    version 1.0 of the session_01 material

--- session_01/README.md
+++ session_01/README.md
@@ -5,3 +5,31 @@
 In this session, we will analyze a dataset consisting of an array of E. coli strains that were grown under different metformin concentrations (0, 50, 100, and 200 mM). The original data was read using the Biospa machine in the lab, and the tables were summarized using an in-house Python script.
 
 Given that we have several hundred strains at four conditions, each with triplicates, we will perform basic analyses on the data. For example, we will check the number of strains, conditions, and replicates per condition using R commands. After this, we will learn how to clean the data by filtering and transforming variables into a usable format. Finally, we will conclude the session by learning the basic steps of creating simple boxplots using ggplot2, and how to modify certain aspects of the plots to suit our needs.
+
+The data looks like this once cleaned.
+
+| **Well** | **Replicate** | **Metformin_mM** | **PG** | **Strain** | **AUC** |
+|:---------|--------------:|-----------------:|-------:|:-----------|--------:|
+| B2       |             1 |              200 |     10 | NT12534    |   0.488 |
+| B3       |             1 |              200 |     10 | NT12540    |   0.004 |
+| B4       |             1 |              200 |     10 | NT12546    |   0.961 |
+| B5       |             1 |              200 |     10 | NT12557    |   0.012 |
+| B6       |             1 |              200 |     10 | NT12010    |   3.510 |
+| B7       |             1 |              200 |     10 | NT12016    |   0.257 |
+
+During the course of the first session, we will learn:
+
+-   What are vectors in R and how to index them
+
+-   What are dataframes in R and how to index them
+
+-   To use the basic functions from `dplyr` such as `filter` or `select`
+
+-   How to make filter multiple items in the data using logical operators (`AND` and `OR`)
+
+-   How to generate very simple dot plots and boxplots
+
+-   A very brief introduction on how to modify the plot (e.g., labels, colours, font size...)
+
+In the end you should be able to generate a plot similar to this:
+[Link to PDF plot](path/to/my_plot.pdf)
--- session_01/code/bacterial_AUCs.R
+++ session_01/code/bacterial_AUCs.R
@@ -1,14 +1,93 @@
 
 # libraries ---------------------------------------------------------------
 
+# how to load variables? 
 library(tidyverse)
 library(here)
 library(cowplot)
 
 
+
+# BASICS ------------------------------------------------------------------
+
+## VECTORS ---------------------------
+
+# Create a vector of numbers from 1 to 5
+my_vector = c(1, 2, 3, 4, 5)
+
+# Print the vector
+print(my_vector)
+my_vector
+
+# Assign a new value to the third element of the vector
+my_vector[3] = 10
+
+# Print the modified vector
+print(my_vector)
+
+# Calculate the mean of the vector
+mean_value = mean(my_vector)
+
+# Print the mean
+print(mean_value)
+
+## DATAFRAME ----------------------------
+
+# Create a vector for the first column
+col1 <- c("A", "B", "C", "D")
+
+# Create a vector for the second column
+col2 <- c(1, 2, 3, 4)
+
+# Create a vector for the third column
+col3 <- c(TRUE, FALSE, FALSE, TRUE)
+
+# Combine the vectors into a data frame
+my_df <- data.frame(col1, col2, col3)
+
+# Print the data frame
+print(my_df)
+
+## INDEXING -----------------------------
+
+# Create a sample data frame
+my_df <- data.frame(col1 = c("A", "B", "C", "D"), 
+                    col2 = c(1, 2, 3, 4), 
+                    col3 = c(TRUE, FALSE, FALSE, TRUE))
+my_df
+
+# Index the first row and second column using row and column numbers
+my_df[1, 2]
+
+# Index the entire second column using the column name
+my_df$col2
+
+# Index the third row using row number
+my_df[3, ]
+
+# Index the first and second columns
+my_df[, c(1,2)]
+
+# Index the rows where col3 is TRUE
+my_df[my_df$col3 == TRUE, ]
+
+# Index the rows where col2 is greater than 2 and col3 is FALSE
+my_df[my_df$col2 > 2 & my_df$col3 == FALSE, ]
+
+
+
+
+
+# BACTERIAL GROWTH --------------------------------------------------------
+
 # load data ---------------------------------------------------------------
 
-auc = read_csv(here('data', 'Output_595', 'Summary.csv'))
+# loading the data with a specific function
+auc = read_csv(here('data', 'Output_595', 'Summary.csv'))  %>% 
+  mutate(Metformin_mM = factor(Metformin_mM, 
+                               levels = c(0,50,100,200)))
+# there are some standards for the coding, as for example not passing certain
+# length per line if you can, as it can make the code very difficult to read
 
 
 head(auc)
@@ -19,9 +98,10 @@ view(auc)
 # data cleaning -----------------------------------------------------------
 
 auc = auc %>% 
-  drop_na(Strain) %>% 
+  drop_na(Strain) %>% # this removes any empty well from our analysis
+  # this selects the columns we want to keep
   select(Well, Replicate, Metformin_mM, PG, Strain, `595nm_f_AUC`) %>% 
-  rename(AUC = `595nm_f_AUC`)
+  rename(AUC = `595nm_f_AUC`) # to rename 
 
 
 # some basic data transformation
@@ -57,40 +137,179 @@ length(unique(auc$Replicate))
 
 # filtering data ---------------------------------------
 
-## simple filtering
+## simple filtering ------------------
 auc %>% 
   filter(Strain == 'NT12060') %>% 
   arrange(Metformin_mM, Replicate)
 
-nt12060 = auc[auc$Strain == 'NT12060',]
-nt12060[order(nt12060$Metformin_mM),]
+# Create a subset of the 'auc' data frame where Strain equals 'NT12060'
+subset_auc = auc[auc$Strain == 'NT12060', ]
+
+# Order the subset by Metformin_mM and Replicate
+ordered_subset_auc = subset_auc[order(subset_auc$Metformin_mM, subset_auc$Replicate), ]
 
 
-
-## multiple filtering
-
+## multiple filtering ----------------
 strains = unique(auc$Strain)
-
 strains[1:3]
 
+# Using tidyverse (dplyr)
 auc %>% 
-  filter(Strain %in% strains[1:3])
+  filter(Strain %in% strains[1:3]) %>% 
+  arrange(Metformin_mM, Replicate)
 
-auc[auc$Strain %in% strains[1:3],]
+# Create a subset of the 'auc' data frame where Strain is in the first three 
+#elements of 'strains'
+subset_auc <- auc[auc$Strain %in% strains[1:3], ]
 
-
-
-
-
-
-
-
-
+# Order the subset by Metformin_mM and Replicate
+ordered_subset_auc <- subset_auc[order(subset_auc$Metformin_mM, 
+                                       subset_auc$Replicate), ]
 
 
 
+## filtering with operators ------------
+
+# filtering one strain and one replicate
+auc %>% 
+  filter(Strain == 'NT12060' & Replicate == 1)
+
+# same effect
+auc %>% 
+  filter(Strain == 'NT12060', Replicate == 1)
+
+# multiple filtering and operators
+auc %>% 
+  filter(Strain == 'NT12060' & Replicate %in% c(1,2))
+
+# filtering  by AUC values, and then also by strains
+auc %>% 
+  filter(AUC > 15)
+
+auc %>% 
+  filter(Strain == 'NT12060' & AUC > 10)
+
+auc %>% 
+  filter(Strain == 'NT12060' | AUC > 10)
+
+
+## Special operators to filter data
+auc %>% 
+  filter(str_detect(Strain, "NT120"))
 
 
 
+
+
+# PLOTS -------------------------------------------------------------------
+
+
+# A very basic plot with ggplot
+
+simple_data = auc %>% 
+  filter(Strain == 'NT12060', Replicate == 1)
+
+simple_data
+
+simple_data %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC)) +
+  geom_point()
+
+
+auc %>% 
+  filter(Strain == 'NT12060', Replicate == 1) %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC)) +
+  geom_point(color = 'red', 
+             size = 4)
+
+# a bit more complex
+# auc %>% 
+#   filter(Strain == 'NT12060', Replicate == 1) %>% 
+#   ggplot(aes(x = Metformin_mM, y = AUC)) +
+#   geom_point(color = 'red', 
+#              size = 4) +
+#   geom_label(aes(label = AUC))
+
+
+
+# let's make an example of a boxplot by isolating one of the Strains
+
+test_boxplot = auc %>% 
+  filter(Strain == 'NT12060')
+
+# the most basic boxplot
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC)) +
+  geom_boxplot()
+
+
+# a bit more colorful
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, color = Metformin_mM)) +
+  geom_boxplot()
+
+
+# let's plot the points
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, color = Metformin_mM)) +
+  geom_point() 
+
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, color = Metformin_mM)) +
+  geom_point() +
+  geom_boxplot()
+# where are the points? !!!
+
+# personal choices...
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, fill = Metformin_mM)) +
+  geom_boxplot() +
+  geom_point(size = 2)
+# points look a bit... meh
+
+# change the x-position of the points
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, fill = Metformin_mM)) +
+  geom_boxplot() +
+  geom_point(size = 2, position = position_jitterdodge())
+
+
+
+
+# a bit more advanced
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, fill = Metformin_mM)) +
+  geom_boxplot(color = 'grey20') +
+  geom_point(size = 2, position = position_jitterdodge()) +
+  labs(
+    x = 'Metformin (mM)',
+    y = 'Area Under the Curve (AUC, arbitrary units)'
+  ) +
+  scale_fill_manual(values = c(
+    '#1911F0',
+    '#14A5F7',
+    '#1DE09E',
+    '#24F714'
+  ), 
+  name = 'Metformin') +
+  theme_half_open(14) +
+  background_grid()
+
+
+test_boxplot %>% 
+  ggplot(aes(x = Metformin_mM, y = AUC, fill = Metformin_mM)) +
+  geom_boxplot(color = 'black') +
+  geom_point(size = 2, position = position_jitterdodge()) +
+  labs(
+    x = 'Metformin (mM)',
+    y = 'Area Under the Curve (AUC, arbitrary units)'
+  ) +
+  viridis::scale_fill_viridis(begin = 0.3, 
+                              discrete = TRUE) +
+  theme_half_open(14) +
+  background_grid()
+
+#### save the plot!!! -------
+ggsave('plots/boxplot_NT12060.pdf', height = 5, width = 6)
 
 
